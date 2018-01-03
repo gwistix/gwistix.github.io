587cd659daef0375393492e49d79909d65b2291f
Chris Duncan <csduncan@gmail.com>
2018-01-02 23:11:11 -0600

    Basic iOS Knopekins Start.

---
+++ ios/Knopekin/Knopekin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Knopekin
+//
+//  Created by Chris Duncan on 1/2/18.
+//  Copyright © 2018 Chris Duncan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
